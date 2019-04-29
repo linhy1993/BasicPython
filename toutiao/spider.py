e579fa8bdb87b8ed2a558bcdd8235bd03b6df874
linhy1993 <linhongyi1110@gmail.com>
2019-04-28 22:37:44 -0400

    add spider for toutiao

---
+++ toutiao/spider.py
@@ -0,0 +1,88 @@
+import re
+from urllib.parse import urlencode
+import requests
+import json
+from bs4 import BeautifulSoup
+from requests.exceptions import RequestException
+
+HEADERS = {
+    'accept': 'application / json, text / javascript',
+    'accept - encoding': 'gzip, deflate, br',
+    'accept - language': 'zh - CN, zh; q = 0.9, en; q = 0.8, fr; q = 0.7',
+    'content - type': 'application / x - www - form - urlencoded',
+    'cookie': 'tt_webid=6684047013610440196; WEATHER_CITY=%E5%8C%97%E4%BA%AC; UM_distinctid=16a57c9646d448-02fe5e946a799e-36697e04-384000-16a57c9646ea52; tt_webid=6684047013610440196; csrftoken=c95a047ca5c73ca0d71ad0ad03e0f9e0; uuid="w:7c98dc6a41934eeab36115a06838ecd7"; __tasessionId=738vudnc31556374308099; CNZZDATA1259612802=278262223-1556248060-https%253A%252F%252Fwww.google.com%252F%7C1556371728; s_v_web_id=3e9f73082247f6d6e0ffc8eb3bbae3cb',
+    'referer': 'https://www.toutiao.com/search/?keyword=%E8%A1%97%E6%8B%8D',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest'
+}
+
+
+def get_page_index(offset, keyword):
+    data = {
+        'aid': 24,
+        'app_name': 'web_search',
+        'offset': offset,
+        'format': 'json',
+        'keyword': keyword,
+        'autoload': 'true',
+        'count': 20,
+        'en_qc': 1,
+        'cur_tab': 1,
+        'from': 'search_tab',
+        'pd': 'synthesis',
+        'timestamp': 1556374347326
+    }
+
+    url = 'https://www.toutiao.com/api/search/content/?' + urlencode(data)
+    try:
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        print('请求索引页面出错')
+        return None
+
+
+def parse_one_page(html):
+    data = json.loads(html)
+    if data and 'data' in data.keys():
+        for item in data.get('data'):
+            yield item.get('article_url')
+
+
+def get_page_detail(url):
+    try:
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        print('请求详情页面出错')
+        return None
+
+
+def parse_get_page(html, url):
+    soup = BeautifulSoup(html, 'lxml')
+    title = soup.select('title')[0].get_text()
+    pattern = re.compile('img src&#x3D;&quot;(.*?)&quot;', re.S)
+    image = re.findall(pattern, html)
+    if image:
+        return {
+            'title': title,
+            'url': url,
+            'image': image
+        }
+
+
+def main():
+    html = get_page_index(0, '街拍')
+    for url in parse_one_page(html):
+        html_detail = get_page_detail(url)
+        if html_detail:
+            result = parse_get_page(html_detail, url)
+            print(result)
+
+
+if __name__ == '__main__':
+    main()
