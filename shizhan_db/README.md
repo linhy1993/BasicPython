2380d866d364a8be77372d43d4b2899a5545581b
linhy1993 <linhongyi1110@gmail.com>
2019-08-02 14:32:16 -0400

    add search function in sql

---
+++ shizhan_db/README.md
@@ -0,0 +1,9 @@
+# Top Movie
+* spider for maoyan movies top 100
+* use mysql database
+
+# function
+* search movie name in top 100
+* search movie by range
+* search movie by score range
+* search movie by year or range
--- shizhan_db/db.py
+++ shizhan_db/db.py
@@ -28,10 +28,52 @@ class Database():
         sql = "TRUNCATE table movies;"
         self._execute(sql=sql)
 
-    def search_name(self, name):
-        sql = "SELECT * FROM movies WHERE name=('%s');" % (name)
+    def search_by_name(self, name):
+        """Search by name"""
+        sql = "SELECT * FROM movies WHERE name = '%s';" % (name)
         self.cursor.execute(sql)
-        results = self.cursor.fetchone()
-        name, stars, release_time, score = results[0], results[1], results[2], results[3]
-        return name, stars, release_time
+        result = self.cursor.fetchone()
+        return result
 
+    def search_by_rank_range(self, rank=0, first=0, last=0):
+        """Search by rank range"""
+        if rank:
+            sql = "SELECT * FROM movies WHERE id = %s;" % rank
+        else:
+            sql = "SELECT * FROM movies WHERE id BETWEEN %s AND %s;" % (first, last)
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+        return results
+
+    def search_by_score_range(self, below=0.0, above=0.0):
+        """Search by score range"""
+        sql = "SELECT * FROM movies WHERE score BETWEEN %s AND %s;" % (below, above)
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+        return results
+
+    def search_by_year(self, year=None, start=None, end=None):
+        """Search by year or year range"""
+        if start and end:
+            sql = "SELECT * FROM movies WHERE YEAR(release_time) BETWEEN %s AND %s;" % (start, end)
+        else:
+            sql = "SELECT * FROM movies WHERE YEAR(release_time) = %s;" % year
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+        return results
+
+
+def test():
+    db = Database()
+    print(f"""
+    search by name                      {db.search_by_name('霸王别姬')}
+    search by rank 1                    {db.search_by_rank_range(rank=1)}
+    search by rank 1 to 2               {db.search_by_rank_range(first=1, last=2)}
+    search by score range 9.6 to 9.7    {db.search_by_score_range(below=9.6, above=9.7)}
+    search by year from 1952 to 1953    {db.search_by_year(start=1952, end=1953)}
+    search by specific year 1953        {db.search_by_year(year=1953)}
+    """)
+
+
+if __name__ == '__main__':
+    test()
