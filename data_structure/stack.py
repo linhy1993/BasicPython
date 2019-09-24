b7cac2061a42e7e0e9b0a001affc201757a10811
linhy1993 <linhongyi1110@gmail.com>
2019-09-24 09:12:43 -0400

    add stack, queue

---
+++ data_structure/stack.py
@@ -0,0 +1,33 @@
+class Stack(object):
+    def __init__(self):
+        self.__list = []
+
+    def push(self, item):
+        """压栈"""
+        self.__list.append(item)
+
+    def pop(self):
+        return self.__list.pop()
+
+    def peek(self):
+        """返回栈顶元素"""
+        if self.__list:
+            return self.__list[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return self.__list == []
+
+    def size(self):
+        return len(self.__list)
+
+if __name__ == '__main__':
+    s=Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.push(4)
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
